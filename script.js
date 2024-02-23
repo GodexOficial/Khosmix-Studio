d3223f0f062e311a6f035943f3b18f2c29f8006d
GodexOficial <matheus66costa887@gmail.com>
2024-02-23 02:00:13 -0300

    Khosmix Studio

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-var num1;
-num1 = 5;
-var num2;
-num2 = 5;
+const menuLinks = document.querySelectorAll('.kheader a[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(distanceFromTheTop) {
+  window.scroll({
+    top: distanceFromTheTop,
+    behavior: "smooth",
+  });
+}
+
+function scrollToDiv(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+  nativeScroll(distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToDiv);
+});
+window.addEventListener("scroll", function () {
+  let scroll = document.querySelector(".button");
+  scroll.classList.toggle("active", window.scrollY > 450);
+});
+function backTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
