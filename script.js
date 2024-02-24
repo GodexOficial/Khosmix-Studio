f9c45c2e9391f48016088eb98ff373cec875267a
GodexOficial <matheus66costa887@gmail.com>
2024-02-23 23:28:28 -0300

    Khosmix Studio

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ function backTop() {
     behavior: "smooth",
   });
 }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("hidden");
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+      entry.target.classList.add("hidden");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
